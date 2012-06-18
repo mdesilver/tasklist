217772932a5ef186cc32857fb2dd5314acabdfb4
mdesilver <mdesilver@hotmail.com>
2012-06-17 20:06:22 -0400

    This is the intial commit with the code

---
+++ app.js
@@ -0,0 +1,45 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes');
+
+var app = module.exports = express.createServer();
+
+// Configuration
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler());
+});
+
+// Routes
+var TaskList = require('./routes/tasklist');
+
+//Windows Mongo deployed through 10gen Package
+// var taskList = new TaskList('mongodb://Administrator:password@168.62.36.133/Mongo');
+
+// Linux Mongo as a Virtual Machine
+var taskList = new TaskList('mongodb://Administrator:password@168.62.161.164/mongo');
+
+
+app.get('/', taskList.showTasks.bind(taskList));
+app.post('/addtask', taskList.addTask.bind(taskList));
+app.post('/completetask', taskList.completeTask.bind(taskList));
+
+
+app.listen(process.env.port || 3000);
